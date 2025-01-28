7ab9d2d245b6309509a93b0a4fdde03a62f3a26d
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-01-28 11:47:08 -0300

    Validando o input

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 const amount = document.getElementById('amount');
 
 amount.addEventListener('input', () => {
+  const hasCharactersRegex = /\D+/g;
+  amount.value = amount.value.replace(hasCharactersRegex, '');
+
   console.log(amount.value);
 })
